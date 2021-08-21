157451e1ce5bf1d650e7bb7382af2d046ef89bd6
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2021-08-21 15:45:12 +0530

    wiring checked

---
+++ app.js
@@ -0,0 +1,8 @@
+var dob = document.querySelector("#date-of-birth");
+var luckyNumber =document.querySelector("#lucky-number");
+var checkBtn = document .querySelector("#check-button");
+
+checkBtn.addEventListener("click", function clickHanlder()
+{
+    console.log("clicked");
+});
