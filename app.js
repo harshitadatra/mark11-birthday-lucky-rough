c32ce2c482b2a13e20f86cf722b01fbddbf87b7c
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2021-08-21 16:15:47 +0530

    javascript done

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
-var dob = document.querySelector("#date-of-birth");
+var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNumber =document.querySelector("#lucky-number");
 var checkBtn = document .querySelector("#check-button");
 
 checkBtn.addEventListener("click", function clickHanlder()
 {
-    console.log("clicked");
+    var dob = dateOfBirth.value;
+    // console.log(dob);
+      var sum =sumOfNumber(dob);
+      checkLucky(sum,luckyNumber.value);
+
+
 });
+function sumOfNumber(dob)
+{
+    var dob= dob.replaceAll("-","");
+    // console.log(dob);
+    let sum = 0;
+    for(let index=0;index<dob.length;index++)
+    {
+        sum=sum + Number(dob.charAt(index));
+    }
+    // console.log(sum);
+    return sum;
+   
+}
+function checkLucky(sum,luckyNumber)
+{
+    if(sum%luckyNumber==0)
+    {
+        console.log("your birthday is lucky 😘");
+    }
+    else{
+        console.log("your birthday is not lucky 😣");
+    }
+}
